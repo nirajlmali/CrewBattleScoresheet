6f3040b77a7d6e9785ccb6529394b296cf8055c1
Niraj Mali <nirajlmali@gmail.com>
2021-01-18 14:56:04 -0800

    Checkpoint for moving to Electron

--- scoresheetscript.py
+++ scoresheetscript.py
@@ -1,8 +1,12 @@
+from tkinter import *
+
 #   #   #   #   #
 #   Inst Vars.  #
 #   #   #   #   #
 endProgram = False
-records = []
+firstRecord = ["Enter Player 1...", "P1 Character", "Stocks Taken", "Enter Player 2...", "P2 Character", "Stocks Taken", "Winner"]
+records = [firstRecord]
+matches = ["1. " + firstRecord[0] + "(" + firstRecord[1] + ")[" + firstRecord[2] + "] vs. [" + firstRecord[5] + "](" + firstRecord[4] + ")" + firstRecord[3]]
 lastTeamWon = 0
 offset = "                             \n"
 score1 = 0
@@ -71,69 +75,123 @@ def writeToScoreSheets(record):
     scoreSheetCrew2.close()
     scoreSheet.close()
 
+
+#   #   #   #   #   
+# Tkinter functions
+#   #   #   #   #
+
+# New Score Sheet
+
+
+
+# Edit
+
+# Finalize
+
+
+#   #   #   #   #   #
+#   Tkinter Window  #
+#   #   #   #   #   #
+
+master = Tk()
+
+variable = StringVar(master)
+variable.set(matches[0]) # default value
+
+w = OptionMenu(master, variable, *matches)
+w.pack()
+
+# New match
+def newMatch():
+    newRecord = ["Enter Player 1...", "P1 Character", "Stocks Taken", "Enter Player 2...", "P2 Character", "Stocks Taken", "Winner"]
+    records.append(newRecord)
+    matches.append(str(len(matches)) + ". " + newRecord[0] + "(" + newRecord[1] + ")[" + newRecord[2] + "] vs. [" + newRecord[5] + "](" + newRecord[4] + ")" + newRecord[3])
+    print(records)
+    w = OptionMenu(master, variable, *matches)
+    w.pack() 
+
+p1Var = firstRecord[0]
+p1Char = firstRecord[1]
+p1Score = firstRecord[2]
+p2Var = firstRecord[3]
+p2Char = firstRecord[4]
+p2Score = firstRecord[5]
+
+p1Entry = Entry(master)
+p1Entry.insert(END, p1Var)
+p1Entry.pack()
+
+addRecordBtn = Button(master, text="+ Add Match", command=newMatch)
+addRecordBtn.pack()
+
+
+# start the loops
+mainloop()
+
 #   #   #   #   #
 #   Loop        #
 #   #   #   #   #
-while not endProgram:
-    print("Command List:\n- N: New Score Sheet\n- I: Insert new match\n- M: Insert Multiple matches\n- F: Finalize Score Sheet(In progress)\n- E: Edit a Match\n- X: Exit program")
-    cmd = input("Enter command: ")
 
-    # Insert New Matches
-    if cmd == 'I' or cmd == 'M':
-        matches = 1
-        if(cmd == 'M'):
-            matches = int(input("How many matches to record: "))
+# while not endProgram:
+#     print("Command List:\n- N: New Score Sheet\n- I: Insert new match\n- M: Insert Multiple matches\n- F: Finalize Score Sheet(In progress)\n- E: Edit a Match\n- X: Exit program")
+#     cmd = input("Enter command: ")
+
+#     # Insert New Matches
+#     if cmd == 'I' or cmd == 'M':
+#         matches = 1
+#         if(cmd == 'M'):
+#             matches = int(input("How many matches to record: "))
         
-        for x in range(matches):
-            record = createRecord()
-            records.append(record)
-            print(records)
-            writeToScoreSheets(record)
+#         for x in range(matches):
+#             record = createRecord()
+#             records.append(record)
+#             print(records)
+#             writeToScoreSheets(record)
 
-    # Start a new score sheet
-    elif cmd == 'N':
-        resetTextFiles()
-        scoreSheet = open("ScoreSheet.txt", 'w')
-        crew1File = open('crew1.txt', 'w')
-        crew2File = open('crew2.txt', 'w')
-        crew1 = input("Enter name of first crew: ")
-        crew2 = input("Enter name of second crew: ")
-        crew1File.write(crew1)
-        crew2File.write(crew2)
-        scoreSheet.write("__**@" + crew1 + " vs. @" + crew2 + "**__\n")
-        crew1File.close()
-        crew2File.close()
+#     # Start a new score sheet
+#     elif cmd == 'N':
+#         resetTextFiles()
+#         scoreSheet = open("ScoreSheet.txt", 'w')
+#         crew1File = open('crew1.txt', 'w')
+#         crew2File = open('crew2.txt', 'w')
+#         crew1 = input("Enter name of first crew: ")
+#         crew2 = input("Enter name of second crew: ")
+#         crew1File.write(crew1)
+#         crew2File.write(crew2)
+#         scoreSheet.write("__**@" + crew1 + " vs. @" + crew2 + "**__\n")
+#         crew1File.close()
+#         crew2File.close()
 
-    # Finalize Score Sheet
-    elif cmd == 'F':
-        winningCrew = input("Winning Crew Name: ")
-        score1 = int(input("Score 1: "))
-        score2 = int(input("Score 2: "))
-        scoreSheet = open("ScoreSheet.txt", 'a')
-        scoreSheet.write("**" + winningCrew + " wins " + str(score1) + "-" + str(score2) + "**")
-        scoreSheet.close()
+#     # Finalize Score Sheet
+#     elif cmd == 'F':
+#         winningCrew = input("Winning Crew Name: ")
+#         score1 = int(input("Score 1: "))
+#         score2 = int(input("Score 2: "))
+#         scoreSheet = open("ScoreSheet.txt", 'a')
+#         scoreSheet.write("**" + winningCrew + " wins " + str(score1) + "-" + str(score2) + "**")
+#         scoreSheet.close()
 
-    elif cmd == 'E':
-        print("Which record to edit?")
-        for i in range(len(records)):
-            match = records[i]
-            print(str(i) + ". " + match[0] + " [" + str(match[2]) + "] vs. [" + str(match[5]) + "] " + match[3] + " - Winner is Crew: " + str(match[6]))
-        userIn = -1
-        while userIn < 0 or userIn > len(records):
-            userIn = int(input("Enter record number: "))
+#     elif cmd == 'E':
+#         print("Which record to edit?")
+#         for i in range(len(records)):
+#             match = records[i]
+#             print(str(i) + ". " + match[0] + " [" + str(match[2]) + "] vs. [" + str(match[5]) + "] " + match[3] + " - Winner is Crew: " + str(match[6]))
+#         userIn = -1
+#         while userIn < 0 or userIn > len(records):
+#             userIn = int(input("Enter record number: "))
 
-        records[userIn] = createRecord()
-        resetTextFiles()
-        scoreSheet = open("ScoreSheet.txt", 'a')
-        scoreSheet.write("__**@" + crew1 + " vs. @" + crew2 + "**__\n")
-        scoreSheet.close()
-        for j in range(len(records)):
-            writeToScoreSheets(records[j])
+#         records[userIn] = createRecord()
+#         resetTextFiles()
+#         scoreSheet = open("ScoreSheet.txt", 'a')
+#         scoreSheet.write("__**@" + crew1 + " vs. @" + crew2 + "**__\n")
+#         scoreSheet.close()
+#         for j in range(len(records)):
+#             writeToScoreSheets(records[j])
 
-    elif cmd == 'X':
-        endProgram = True
+#     elif cmd == 'X':
+#         endProgram = True
     
-    else:
-        print("Invalid command")
+#     else:
+#         print("Invalid command")
 
     
